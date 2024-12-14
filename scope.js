72acd7c4fe5399e71c2a24d0959cd746c3504df0
fathimamarsheenakp <fathimamarsheena@gmail.com>
2024-12-14 16:38:17 +0530

    Updated scope.js

--- scope.js
+++ scope.js
@@ -48,3 +48,27 @@ console.log("Accessing in globally");
 console.log("a = " + a);
 // console.log("b = " + b);     cannot acces
 // console.log("c = " + c);     cannot acces
+console.log();
+
+var aa = 10; // global scope
+function outer() {
+    var bb = 20;
+    function inner() {
+        console.log("inner() executing");
+        var cc = 30;
+        console.log("a = " + aa);
+        console.log("b = " + bb);
+        console.log("c = " + cc); 
+    }
+    console.log("outer() executing");
+    console.log("a = " + aa);
+    console.log("b = " + bb);
+    // console.log("c = " + cc);    cannot acces
+    inner();
+}
+outer();
+console.log("global access");
+    console.log("a = " + aa);
+    // console.log("b = " + bb);    cannot acces
+    // console.log("c = " + cc);    cannot acces
+
