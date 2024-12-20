166ebd30e74ddc3d25bcdd10841a8fad6b83f981
fathimamarsheenakp <fathimamarsheena@gmail.com>
2024-12-20 10:14:50 +0530

    Added Synchronization.js

---
+++ Synchronization.js
@@ -0,0 +1,10 @@
+console.log("Instruction 1");
+console.log("Instruction 2");
+console.log("Instruction 3");
+console.log();
+
+console.log("Instruction 1");
+setTimeout(function() {
+    console.log("Hello");
+})
+console.log("Instruction 3");
