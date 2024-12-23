10a114aca5e2b656f6850b74ba2234b70714b781
fathimamarsheenakp <fathimamarsheena@gmail.com>
2024-12-23 19:30:12 +0530

    Updated Register

--- Assignments/Register.js
+++ Assignments/Register.js
@@ -1,5 +1,10 @@
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
+
+    if (!validAll()) {
+        return; // Stop form submission if validation fails
+    }
+
     alert("Form submitted!");
     
     us = document.querySelector('#uname').value;
@@ -16,3 +21,44 @@ document.querySelector('form').addEventListener('submit', (event) => {
         input.value = ""; // Clear the value of each input field
     });
 });
+
+function validAll() {
+    usname = document.querySelector('#uname').value.trim();
+    email = document.querySelector("#email").value.trim();
+    password = document.querySelector("#password").value.trim();
+    
+    usernamePattern = /^[a-zA-Z]{4,}$/;
+    emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/;
+    passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/
+
+    if (!usname) {
+        alert("Username is required.");
+        return false;
+    }
+
+    if (!usernamePattern.test(usname)) {
+        alert("Please enter a valid username.");
+        return false;
+    }
+
+    if (!email) {
+        alert("Email is required.");
+        return false;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (!password) {
+        alert("Password is required.");
+        return false;
+    }
+    
+    if (!passwordPattern.test(password)) {
+        alert("Password should contain atleast one capital letter, small letter and symbol.");
+        return false;
+    }
+    return true;
+}
